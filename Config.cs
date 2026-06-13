157c6fd1434f767a349ced055b4382ff6a5d0ae1
NerdGarage <111273913+NerdGarage43@users.noreply.github.com>
2026-06-13 13:48:03 -0400

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
+namespace Owendraw
+{
+    public class Config : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+
+        [LabelKey("$Mods.Owendraw.Config.DisableDyes")]
+        [DefaultValue(true)]
+        public bool DisableDyes { get; set; }
+    }
+}
---
+++ Owendraw.cs
@@ -0,0 +1,9 @@
+using Terraria.ModLoader;
+
+namespace Owendraw
+{
+	public class Owendraw : Mod
+	{
+
+	}
+}
